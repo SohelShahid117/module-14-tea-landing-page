8793566988ad044a9b9d5e2dbbe3671a289b8c1b
SohelShahid117 <sohelshahid09@gmail.com>
2024-01-20 23:47:37 +0600

    coding complete of tea landing page up to 14-3

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+//https://tailwindcss.com/docs/installation--->Tailwind CLI teke nicher file t nite hobe
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
